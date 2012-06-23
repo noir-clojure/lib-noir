c0e4193dc75dea95a43e2949050b62d745c254ae
Anthony Grimes <i@raynes.me>
2012-06-23 16:13:17 -0500

    Add license info.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject lib-noir "0.1.0"
   :description "Libraries from Noir for your enjoyment."
   :url "http://webnoir.org"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "4.0.0"]
                  [ring "1.1.1"]
