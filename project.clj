5cdaef31af1181ad0e4b0975eda6a6fe727609c4
Anthony Grimes <i@raynes.me>
2012-12-31 15:40:34 -0600

    Inc version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lib-noir "0.3.1"
+(defproject lib-noir "0.3.2"
   :description "Libraries from Noir for your enjoyment."
   :url "http://webnoir.org"
   :license {:name "Eclipse Public License - v 1.0"
