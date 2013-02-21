1675116d7268106f905716b67ad7c1308a3bd777
Yogthos <dmitri.sotnikov@gmail.com>
2013-02-21 10:57:17 -0500

    updated dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cheshire "5.0.1"]
-                 [ring "1.1.1"]
-                 [compojure "1.1.3"]
+                 [cheshire "5.0.2"]
+                 [ring "1.1.8"]
+                 [compojure "1.1.5"]
                  [hiccup "1.0.2"]
                  [org.mindrot/jbcrypt "0.3m"]]
   :plugins [[codox "0.6.4"]]
