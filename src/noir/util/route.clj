8e21998a7001d10af48c71d037bf4ea3a2717818
Yogthos <dmitri.sotnikov@gmail.com>
2013-02-21 00:25:37 -0500

    updated to allow setting a custom redirect URI for access rules middleware

--- src/noir/util/route.clj
+++ src/noir/util/route.clj
@@ -7,8 +7,13 @@
    if no rules match then the response is a redirect to \"/\""
   [method url params & body]
   `(~method ~url ~params
-     (let [rules# (:access-rules noir.request/*request*)]
+     (let [[x# & xs# :as items#] (:access-rules noir.request/*request*)
+           redirect# (if (map? x#) (:redirect x#) "/")
+           rules#    (if (map? x#) xs# items#)]       
        (if (or (nil? rules#)
                (some (fn [rule#] (rule# '~method ~url ~params)) rules#))
          (do ~@body)
-         (noir.response/redirect "/")))))
+         (noir.response/redirect redirect#)))))
+
+
+
