8a626bf88043b2631ea0ab14e2bfaac8be5578e8
Yogthos <dmitri.sotnikov@gmail.com>
2013-04-30 08:17:19 -0400

    bumped versions

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cheshire "5.0.2"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.1.1"]
                  [ring "1.1.8"]
                  [compojure "1.1.5"]
                  [clout "1.1.0"]
                  [hiccup "1.0.3"]
                  [org.mindrot/jbcrypt "0.3m"]]
   :plugins [[codox "0.6.4"]]
-  :codox {:output-dir "doc/0.5.0"})
+  :codox {:output-dir "doc/0.5.1"})
