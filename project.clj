16f75cf6985629c2d38dee5fb45b28030cbffae9
Yogthos <dmitri.sotnikov@gmail.com>
2013-05-21 18:00:52 -0400

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lib-noir "0.5.5"
+(defproject lib-noir "0.5.6"
   :description "Libraries from Noir for your enjoyment."
   :url "https://github.com/noir-clojure/lib-noir"
   :license {:name "Eclipse Public License - v 1.0"
