6a2fed82d5afeb2714525847f9362c47297cffb7
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-08-12 18:43:52 -0400

    bumped versions of hiccup and ring

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.2.0"]
-                 [ring "1.1.8"]
+                 [ring "1.2.0"]
                  [compojure "1.1.5"]
                  [clout "1.1.0"]
-                 [hiccup "1.0.3"]
+                 [hiccup "1.0.4"]
                  [org.mindrot/jbcrypt "0.3m"]]
   :plugins [[codox "0.6.4"]]
   :codox {:output-dir "doc"})
