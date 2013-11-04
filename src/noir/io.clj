7194aee5091539c72bf54099882b481db1e21b4e
Yogthos <dmitri.sotnikov@gmail.com>
2013-11-04 09:00:28 -0500

    updated io/upload-file to use a system relative path

--- src/noir/io.clj
+++ src/noir/io.clj
@@ -8,17 +8,24 @@
   (if-let [path (io/resource (str "public" File/separator))]
     (.getPath path)))
 
-(defn- file-path [relative-path filename]
+(defn- file-path [path & [filename]]
   (java.net.URLDecoder/decode
-    (str (resource-path) relative-path File/separator filename)
+    (str path File/separator filename)
     "utf-8"))
 
+(defn create-path [path create-path?]
+  (let [working-dir  (File. (file-path path))]
+    (when (and create-path? (not (.exists working-dir)))
+      (.mkdirs working-dir))))
+
 (defn upload-file
-  "uploads a file to the public folder of the application"
-  [relative-path {:keys [tempfile size filename size]}]  
-  (try 
+  "uploads a file to the target folder
+   when :create-path? flag is set to true then the target path will be created"
+  [path {:keys [tempfile size filename size]} & {:keys [create-path?]}]
+  (create-path path create-path?)
+  (try
     (with-open [in (new FileInputStream tempfile)
-                out (new FileOutputStream (file-path relative-path filename))]
+                out (new FileOutputStream (file-path path filename))]
       (let [source (.getChannel in)
             dest   (.getChannel out)]
         (.transferFrom dest source 0 (.size source))))))
@@ -28,14 +35,14 @@
    expects path to be a / separated string relative to the public folder, eg:
    (get-resource \"/css/screen.css\" )"
   [relative-path]
-  (if relative-path
+  (when relative-path
     (->> relative-path
          (str "public")
          (io/resource))))
 
 (defn slurp-resource
-  "Opens a reader on f and reads all its contents, returning a string. 
+  "Opens a reader on f and reads all its contents, returning a string.
    Path is specified the same way as for get-resource"
   [path]
-  (if-let [resource (get-resource path)] 
+  (if-let [resource (get-resource path)]
     (-> resource io/input-stream slurp)))
