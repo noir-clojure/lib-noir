ae80c9c87306f85a099217384c3ab6ee75ae698f
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-06-16 11:16:58 -0400

    bumped up ring dependency, added lein-ancient

--- project.clj
+++ project.clj
@@ -6,12 +6,13 @@
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
-                 [ring "1.2.0"]
+                 [ring "1.3.0"]
                  [compojure "1.1.8"]
                  [clout "1.2.0"]
                  [hiccup "1.0.5"]
                  [ring-middleware-format "0.3.2"]
                  [ring/ring-session-timeout "0.1.0"]
                  [org.mindrot/jbcrypt "0.3m"]]
-  :plugins [[codox "0.8.0"]]
+  :plugins [[codox "0.8.9"]
+            [lein-ancient "0.5.5"]]
   :codox {:output-dir "doc"})
