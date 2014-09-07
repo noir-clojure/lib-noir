7f0aa993b856aae0a97dfd38022b5487cdc84ad0
Yogthos <dmitri.sotnikov@gmail.com>
2014-09-07 12:24:58 -0400

    added helper function to clear expired sessions

--- src/noir/session.clj
+++ src/noir/session.clj
@@ -7,12 +7,23 @@
   (:use ring.middleware.session
         ring.middleware.session.memory
         ring.middleware.flash
-        [ring.middleware.session-timeout :only [wrap-idle-session-timeout]]))
+        [ring.middleware.session-timeout
+         :only [wrap-idle-session-timeout]]))
 
 ;; ## Session
 
 (declare ^:dynamic *noir-session*)
+
 (defonce mem (atom {}))
+
+(defn- current-time []
+  (quot (System/currentTimeMillis) 1000))
+
+(defn- expired? [[id session]]
+  (pos? (- (:ring.middleware.session-timeout/idle-timeout session) (current-time))))
+
+(defn clear-expired-sessions []
+  (clojure.core/swap! mem #(->> % (filter expired?) (into {}))))
 
 (defn put!
   "Associates the key with the given value in the session"
